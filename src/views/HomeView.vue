85c33c7be0945d561e4187fb444f8405778b85e4
passor <passor.catic@gmail.com>
2022-08-30 17:01:19 +0200

    point-de-depart

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Bienvenu sur lee projet commun de Paco et Florian." />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
