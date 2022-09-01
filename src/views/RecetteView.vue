485fbced2e1545399adef2867039115ae7d81048
aden <aden@formation.com>
2022-09-01 14:06:55 +0200

    TresSale2

--- src/views/RecetteView.vue
+++ src/views/RecetteView.vue
@@ -1,28 +1,29 @@
 <template>
     <div class="blockRecipeImage">
-        <h1 class="nameRecipe">Nom Recette</h1>
-        <div id="image">
-            <h3>IMAGE DE LA RECETTE</h3>
+        <h1 class="nameRecipe">Les recettes</h1>
+        <div id="recipes">
+            <ul class="recipeCell">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
         </div>
-        <div id="recipe">
+
+        <!-- <div id="recipe">
             <p>
                 <span v-for="(element, index) of $store.getters.listRecipe " :key="index">{{ element }}&nbsp;</span>
             </p>
-            <!-- <input type="button" @click="addRecipe" value="Ajouter une recette"> -->
+            <input type="button" @click="addRecipe" value="Ajouter une recette">
             <input type="button" @click="goToPage" value="Retour">
-        </div>
-    </div>
-    <div id="step">
-        <h3>ETAPE</h3>
+        </div> -->
     </div>
     <footer id="footer">
         Pied de page
     </footer>
 </template>
-
-
-
-
 
 <script>
 export default {
@@ -46,22 +47,43 @@ export default {
     border-bottom: 1px solid black;
 }
 
-#image {
+#recipeCell {
     border-bottom: 1px solid black;
 }
 
-#step {
-    position: relative;
-    align-content: center;
-    padding-bottom: 100%;
+ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 20px;
+    padding: 0.3em;
 }
 
-#recipe {
-    border-bottom: 1px solid black;
+ul.recipeCell {
+    width: 400px;
+    height: 300px;
+    margin: 0 auto;
+}
+
+ul.recipeCell li {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    background: whitesmoke;
+    display: inline-block;
+    cursor: grab;
+    border: 2px solid rgb(0, 0, 0);
+}
+
+#recipes {
+    padding-bottom: 15vh;
+    padding-top: 10vh;
 }
 
 #footer {
+    display: inline-block;
     border: 1px solid black;
-    padding-bottom: 35%;
+    position: fixed;
+    bottom: 0;
+    align-items: center;
 }
 </style>
