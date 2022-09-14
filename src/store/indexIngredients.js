d93730756e775ff84c40e95a64acb6c32d4c5922
passor <passor.catic@gmail.com>
2022-09-14 12:05:58 +0200

    index lire ingredient de la recette

--- src/store/indexIngredients.js
+++ src/store/indexIngredients.js
@@ -24,16 +24,13 @@ export default {
             var myHeaders = new Headers();
             myHeaders.append("Authorization", "Bearer keyaN8glHhIloJltf")
             let options = { headers: myHeaders }
-            fetch("https://api.airtable.com/v0/appT0bvntx0RS1M8p/Recette_has_Ingredient", options)
+            fetch("https://api.airtable.com/v0/appT0bvntx0RS1M8p/Recette", options)
                 .then(data => data.json())
                 .then(data => {
-                    /* console.log("data records")
-                     context.commit(data.records)
-                     console.log(data.records)*/
                     let tingredient = [];
                     for (let index = 0; index < data.records.length; index++) {
                         const db_record = data.records[index];
-                        tingredient.push({ id: db_record.id, ingredient_id: [...db_record.fields.Ingredient], quantity: db_record.fields.Quantity, unit: db_record.fields.Unit })
+                        tingredient.push({ id_recette: db_record.id, id_ingredient: db_record.fields.Recette_has_Ingredient })
                     }
                     console.log("VUEX: Initialisation des ingredients")
                     console.log(tingredient)
